4057869f331977ac1ed2106a6793dac9fa48a733
wjcho8 <wjcho8@github.com>
2020-09-15 17:55:34 -0500

    Initial Commit

---
+++ GoogleNote/src/main/java/com/rigelsky/GoogleNoteApplication.java
@@ -0,0 +1,13 @@
+package com.rigelsky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleNoteApplication.class, args);
+	}
+
+}
---
+++ GoogleNote/src/test/java/com/rigelsky/GoogleNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigelsky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
